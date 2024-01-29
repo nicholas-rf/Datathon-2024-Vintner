01817376f4a6fed327af75dd611943c215ebd9f2
nicholas-rf <nicholasrozefreitas@gmail.com>
2024-01-29 15:15:53 -0800

    Updates

--- Modules/wine_recommender.py
+++ Modules/wine_recommender.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
+import pickle
 
 # SAMPLE 236       3.5     3.5          4       2.5        red
 # SAMPLE 235         3       3          4         2        red
@@ -12,24 +13,7 @@ import numpy as np
 This module contains the recommender system for the wine based off of a euclidian K-nearest-neighbors algorithm with cosine-similarity used to add variety to the recommendations. 
 """
 
-#example data
-wines = np.array([
-    [5, 2, 4, 0, 3],
-    [3, 5, 2, 5, 3],  
-    [3, 5, 0, 5, 2]  
-])
-
-scalar = StandardScaler()
-wines_scaled = scalar.fit_transform(wines)
-knn.fit(wines_scaled)
-
-user_preference = np.array([["prefs go here"]])
-user_preferance_scaled = scalar.transform(user_preference)
-
-distances, indices = knn.kneighbors(user_preferance_scaled)
-
-
-def set_up_knn(neighbors=20):
+def set_up_knn(neighbors=5):
     """
     Sets up a k-nearest-neighbors model to provide recommendations from for a session.
     
@@ -39,17 +23,29 @@ def set_up_knn(neighbors=20):
     Returns:
         knn (sklearn.neighbors.NearestNeighbors) : A knn model for recommendation gathering.
     """
-    scalar = StandardScaler()
-    wines = pd.read_csv('/Users/nick/Documents/GitHub/spingle-dingle/scripts/users.csv') # change later
-    wines_scaled=scalar.fit_transform(wines) # Apply a scalar so that mean and std get normalized
-    knn = NearestNeighbors(n_neighbors=neighbors, metric='euclidian')
-    knn.fit(wines_scaled)
-    return knn
+    # scalar = StandardScaler()
+    wines = pd.read_csv('/Users/nick/Documents/GitHub/spingle-dingle/feature_data.csv') # change later
+    wines['type'] = wines['type'].apply(lambda x : 0 if x=='red' else 1) # dummy code variables
+    wines.drop(columns=["Unnamed: 0"], inplace=True)
+    # wines_scaled=scalar.fit_transform(wines) # Apply a scalar so that mean and std get normalized
+    knn = NearestNeighbors(n_neighbors=neighbors, metric='euclidean')
+    knn.fit(wines)
+    return knn, wines
 
 def make_request():
-    knn = set_up_knn()
-    request_catalogue = lambda user_info : knn.kneighbors(user_info)
-    wines = request_catalogue(user_info="info here")
-    similarity_mat = cosine_similarity(wines)
+    knn, wines = set_up_knn()
+    feature_vectors = knn._fit_X 
+
+    print(feature_vectors)
+    # with open('/Users/nick/Documents/GitHub/spingle-dingle/data/knn_vectors.pk1', 'wb') as f:
+    #     pickle.dump((feature_vectors), f)
+
+    # _, indices = knn.kneighbors([[0, 6, 1, 4, 4, 0, 3]])
+    # recommended_wines = wines.iloc[indices[0]]  # Use indices to retrieve wines
+    # print(recommended_wines)
+    # request_catalogue = lambda user_info : knn.kneighbors(user_info)
+    # distance, indices = request_catalogue(user_info=[[0, 6, 1, 4, 4, 0, 3]])
+    # similarity_mat = cosine_similarity(wines)
 
     # now to figure out how to get a variety of cosine values, maybe use binning on the similarity vectors? 
+
